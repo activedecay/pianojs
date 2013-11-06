6f46544981533be73c5bf7805b6e43b8e1e808c2
U-squee\jfaust <jfaust@squee.(none)>
2013-11-06 08:38:12 -0700

    keyboard any size. scale iterations to match keyboard size. diatonics

--- js/main.js
+++ js/main.js
@@ -13,6 +13,13 @@ function getScales() {
     return fewerScales;
 }
 
+var MAX_ITERATIONS = 3;
+
+var ACCIDENTAL_LEFT_BEGIN = 14;
+var NOTE_WIDTH = 19;
+var keyboardKeys = 48;
+
+var iterations = 1;
 var accidentalIdx = [1,3,  6,8,10];
 var wholeIdx =     [0,2,4,5,7,9,11];
 var keyboard = [];
@@ -26,13 +33,11 @@ var lastPicked;
 var root = 0;
 var upArrowKey = 38;
 var downArrowKey = 40;
-var minorChordName = "Minor";
+var pageUpKey = 33;
+var pageDownKey = 34;
+var minorChordName = "Natural Minor";
 var majorChordName = "Major";
 
-function noteFromInt(i) {
-    i += 2; // 0 is C
-    return String.fromCharCode(i % 7 + 65); // 65 is 'A'
-}
 
 function createIntervals() {
     stepIntervals = [];
@@ -58,33 +63,12 @@ function createNotes() {
     }
 }
 
-function semitonesKeyupEvent(event) {
-    var semitonesTxtbox = $('.semitones');
-    var semitones = semitonesTxtbox.val();
-    var number = new Number(semitones);
-    if (isNaN(number)) number = 0;
-
-    if (event.which === upArrowKey) {
-        number++;
-        var max = stepIntervals.length - 1;
-        number = number > max ? max : number;
-    }
-    if (event.which === downArrowKey) {
-        number--;
-        number = number <= 0 ? 0 : number;
-    }
-
-    semitonesTxtbox.val(number);
-    semitones = semitonesTxtbox.val();
-    $('.semitones_results').text(stepIntervals[semitones].join(" & "));
-}
-
 function createKeyboard() {
     var wholeLeft = 0;
-    var accidLeft = 14;
+    var accidLeft = ACCIDENTAL_LEFT_BEGIN;
     var accidCounter = 0;
-    var delta = 19;
-    for (var i = 0; i < 48; i++) {
+    var delta = NOTE_WIDTH;
+    for (var i = 0; i < keyboardKeys; i++) {
         var key = $('<div></div>');
         if ($.inArray(i % 12, wholeIdx) !== -1) {
             $(key).addClass('key');
@@ -108,15 +92,117 @@ function createKeyboard() {
 }
 
 function clearKeyboard() {
+    $('.keyboard div').remove();
+    keyboard = [];
+}
+
+function clearKeyboardPressed() {
     $.each(keyboard, function (i) {
         $(keyboard[i]).removeClass('pressed');
     });
 }
 
+function noteFromInt(i) {
+    i += 2; // 0 is C
+    return String.fromCharCode(i % 7 + 65); // 65 is 'A'
+}
+
+function notesInScale(notes, rootIdx, scale, iterations) {
+    if (iterations === undefined) {
+        iterations = 2;
+    }
+
+    var idxs = idxInScale(rootIdx, scale, iterations);
+    var out = [];
+    for (var i = 0; i < idxs.length; i++) {
+        var idx = idxs[i];
+        out.push(notes[idx % notes.length]);
+    }
+    return out;
+}
+
+function idxInScale(idx, scale, iterations) {
+    if (iterations === undefined) {
+        iterations = 2;
+    }
+
+    var out = [idx];
+    for (var i = 0; i < scale.length * iterations; i++) {
+        idx += scale[i % scale.length];
+        out.push(idx);
+    }
+    return out;
+}
+
+function pushKeyboardKeys(chosen, iterations, idx) {
+    var iterated = [];
+    for (var i = 0; i < chosen.length * iterations; i++) {
+        idx += chosen[i % chosen.length];
+        $(keyboard[idx]).addClass('pressed');
+        if (iterations > 1
+            && (i % chosen.length) + 1 == chosen.length
+            && iterated[i / chosen.length] === undefined) {
+            idx = (12 * (iterated.length + 1)) + root;
+            iterated.push(true);
+            $(keyboard[idx]).addClass('pressed');
+        }
+    }
+}
+
+function keyWhichIs(key, e) {
+    return key === String.fromCharCode(e.which);
+}
+
+function getNumberFromText(text, event, max, min) {
+    var number = new Number(text);
+    if (isNaN(number)) number = min;
+
+    if (event.which === upArrowKey) { number++; }
+    if (event.which === downArrowKey) { number--; }
+    if (event.which === pageUpKey) { number+=12; }
+    if (event.which === pageDownKey) { number-=12; }
+    number = number > max ? max : number;
+    number = number <= min ? min : number;
+    return number;
+}
+
+// EVENTS
+function semitonesKeyupEvent(event) {
+    var semitonesTxtbox = $('.semitones');
+    var semitones = semitonesTxtbox.val();
+    var number = getNumberFromText(semitones, event,
+        stepIntervals.length - 1, 0);
+    semitonesTxtbox.val(number);
+    $('.semitones_results').text(stepIntervals[number].join(" & "));
+}
+
+function iterationsKeyupEvent(event) {
+    var iterationsTxtbox = $('.iterations');
+    var textValue = iterationsTxtbox.val();
+    var number = getNumberFromText(textValue, event, keyboardKeys/12, 1);
+
+    iterationsTxtbox.val(number);
+    iterations = number;
+
+    if (lastPicked) lastPicked();
+}
+
+function keyboardKeysKeyupEvent(event) {
+    var keyboardKeysTxtbox = $('.keyboardKeys');
+    var textValue = keyboardKeysTxtbox.val();
+    var number = getNumberFromText(textValue, event, 144, 12);
+
+    keyboardKeysTxtbox.val(number);
+    keyboardKeys = number;
+
+    clearKeyboard();
+    createKeyboard();
+}
+
 function chordsChangeEvent() {
     var chosen = chords[$('.chords option:checked').val()];
     if (!chosen) return;
-    clearKeyboard();
+    clearKeyboardPressed();
     var fout = "";
     var sout = "";
     var iout = "";
@@ -138,58 +224,63 @@ function chordsChangeEvent() {
     lastPicked = chordsChangeEvent;
 }
 
-function notesIn(notes, idx, chosen, iterations) {
-    if (iterations === undefined) {
-        iterations = 2;
-    }
-
-    var out = [notes[idx % notes.length]];
-    for (var i = 0; i < chosen.length * iterations; i++) {
-        idx += chosen[i % chosen.length];
-        out.push(notes[idx % notes.length]);
-    }
-    return out;
-}
-
-function pushKeyboardKeys(chosen, iterations, idx) {
-    var iterated = [];
-    for (var i = 0; i < chosen.length * iterations; i++) {
-        idx += chosen[i % chosen.length];
-        $(keyboard[idx]).addClass('pressed');
-        if (iterations > 1
-            && (i % chosen.length) + 1 == chosen.length
-            && iterated[i / chosen.length] === undefined) {
-            idx = (12 * (iterated.length + 1)) + root;
-            iterated.push(true);
-            $(keyboard[idx]).addClass('pressed');
-        }
-    }
-}
-
 function scalesChangeEvent() {
-    var chosen = scales[$('.scales option:checked').text()];
+    var chosen = scales[$('.scales option:checked').val()];
     if (!chosen) return;
-    clearKeyboard();
-    var iterations = 2;
+    clearKeyboardPressed();
     $(keyboard[root]).addClass('pressed');
     pushKeyboardKeys(chosen, iterations, root);
-    var fout = notesIn(flats, root, chosen, iterations).join(" ");
+    var fout = notesInScale(flats, root, chosen, iterations).join(" ");
     $('.with_flats').text(fout);
-    var sout = notesIn(sharps, root, chosen, iterations).join(" ");
+    var sout = notesInScale(sharps, root, chosen, iterations).join(" ");
     $('.with_sharps').text(sout);
 
     $('.with_intervals').text("");
 
+    updateDiatonic();
+
     lastPicked = scalesChangeEvent;
+}
+
+function updateDiatonic() {
+    var scale = scales[$('.scales option:checked').val()];
+    var chord = chords[$('.chords option:checked').val()];
+    if (!scale || !chord) return;
+    var idxs = idxInScale(root, scale, 4);
+    var chordSpan = chord.length * 2;
+    var keyboardIdxs = [];
+    var diatonicQualities = [];
+    for (var roman = 0; roman < 8; roman++) {
+        var triad = [];
+        // find keyboard indexes
+        for (var i = 0; i < chordSpan; i += 2) {
+            triad.push(idxs[roman + i]);
+        }
+        keyboardIdxs.push(triad);
+        // find triad quality
+        var quality = [];
+        var triadRoot = triad[0];
+        for (i = 0; i < triad.length; i++) {
+            quality.push(triad[i] - triadRoot);
+        }
+        diatonicQualities.push(quality);
+    }
+    var diatonicSelect = $('.diatonic');
+    diatonicSelect.find('option').remove();
+    for (var q in diatonicQualities) {
+        var quality = diatonicQualities[q];
+        for (var obj in chords) {
+            if ($(quality).not(chords[obj]).length == 0
+                    && $(chords[obj]).not(quality).length == 0) {
+                diatonicSelect.append($('<option>'+obj+'</option>'))
+            }
+        }
+    }
 }
 
 function notesChangeEvent() {
     root = flats.indexOf($('.notes option:checked').val());
     if (lastPicked) lastPicked();
-}
-
-function keyWhichIs(key, e) {
-    return key === String.fromCharCode(e.which);
 }
 
 $(document).ready(
@@ -201,6 +292,8 @@ $(document).ready(
         $('.flats').text(flats);
 
         $('.semitones').keyup(semitonesKeyupEvent);
+        $('.iterations').keyup(iterationsKeyupEvent);
+        $('.keyboardKeys').keyup(keyboardKeysKeyupEvent);
 
         //$('.stepIntervals').text(JSON.stringify(stepIntervals, null, ' '));
 
@@ -230,9 +323,11 @@ $(document).ready(
         $('.scales option').each(function (e, a) {
             if (/^Major$/i.test($(a).val())) {
                 $(a).attr('value', majorChordName);
+                $(a).text('Major (alt+M)');
             }
             if (/^Natural Minor$/i.test($(a).val())) {
                 $(a).attr('value', minorChordName);
+                $(a).text('Natural Minor (alt+N)');
             }
         });
         scalesSelectbox.change(scalesChangeEvent);
